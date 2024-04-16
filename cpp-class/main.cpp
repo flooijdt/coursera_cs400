2f637b9be2e6b6637a70206db82cc9e83a23fe11
flooijdt <flooijdt@posteo.de>
2024-04-16 16:23:55 -0300

    default commit.

--- cpp-class/main.cpp
+++ cpp-class/main.cpp
@@ -1,18 +1,18 @@
 /**
  * C++ code for creating a Cube of length 2.4 units.
  * - See ../cpp-std/main.cpp for a similar program with print statements.
- * 
+ *
  * @author
  *   Wade Fagen-Ulmschneider <waf@illinois.edu>
  */
 
-#include <iostream>
 #include "Cube.h"
+#include <iostream>
 
 int main() {
   Cube c;
 
-  c.setLength(3.48);
+  // c.setLength(3.48);
   double volume = c.getVolume();
   std::cout << "Volume: " << volume << std::endl;
 
